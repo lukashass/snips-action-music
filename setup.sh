27b66b9065664cfd4b140b2f947e49645988b7e3
Lukas Hass <lukas@slucky.de>
2019-11-24 23:56:13 +0100

    use hermes config file, update hermes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
 npm install && npm run build
 
 if [ ! -e "./config.ini" ]
 then
-    cp config.ini.default config.ini
+  cp config.ini.default config.ini
+fi
+
+if [ -e "/etc/hermes-config.json" ]
+then
+  ln -s /etc/hermes-config.json hermes-config.json
 fi
